3f1c96b95c5db6bac65e4337aa5ad612db6f5e7f
Fabian2023 <fasalpe34@gmail.com>
2025-09-04 12:25:34 -0500

    cambios en admin sin regreso

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import { storage } from "../firebase/firebase";
 import { ref, listAll, getDownloadURL, deleteObject } from "firebase/storage";
-import { useNavigate } from "react-router-dom";
+//import { useNavigate } from "react-router-dom";
 
 const Admin = () => {
   const [photos, setPhotos] = useState([]);
   const [selectedPhoto, setSelectedPhoto] = useState(null);
   const [confirmDelete, setConfirmDelete] = useState(null); // 🔹 Foto pendiente de eliminar
-  const navigate = useNavigate();
+//   const navigate = useNavigate();
 
   // 🔹 Cargar fotos
   const fetchPhotos = async () => {
@@ -68,12 +68,12 @@ const Admin = () => {
      style={{ backgroundImage: "url('/anillos.jpg')" }}
     >
       {/* Botón Volver */}
-      <button
+      {/* <button
         onClick={() => navigate("/choose")}
         className="mb-4 px-4 py-2 bg-purple-400/70 text-white rounded-lg shadow-md hover:bg-purple-600 transition"
       >
         Regresar
-      </button>
+      </button> */}
 
       <h1 className="text-3xl font-bold text-white mb-6 mt-8 text-center">
         Dashboard Admin
