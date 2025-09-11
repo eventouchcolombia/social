d9d622e635aabd0263c2d3a38e6c4a9831635d6f
Fabian2023 <fasalpe34@gmail.com>
2025-09-11 13:22:31 -0500

    button2

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -10,13 +10,13 @@ const Welcome = () => {
       className="flex flex-col items-center justify-between min-h-screen bg-cover bg-center px-4"
       style={{ backgroundImage: `url('${getAssetUrl("background.jpg")}')` }}
     >
-      <h1 className="text-4xl font-bold text-black text-center mt-8">
+      <h1 className="text-4xl font-bold text-[#BFA065] text-center mt-8">
         EventPhotos
       </h1>
 
       <div className="mb-16">
         <button
-          className="bg-yellow-100 text-yellow-800 px-4 py-2 w-44 h-14 font-semibold rounded-2xl text-2xl shadow-xl hover:bg-yellow-300 transition"
+          className="bg-yellow-100 text-[#BFA065] px-4 py-2 w-56 h-14 font-bold rounded-3xl text-2xl shadow-xl hover:bg-yellow-300 transition"
           onClick={() => navigate(`/${eventSlug}/choose`)}
         >
           Comenzar!
